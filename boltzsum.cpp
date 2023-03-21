23f2cba8be91c4b25d3a1c9ce2d92b7c70989609
Arman Babakhani <armanxbabakhani@berkeley.edu>
2023-03-21 00:46:08 -0700

    adjusted comments

--- boltzsum.cpp
+++ boltzsum.cpp
@@ -29,7 +29,7 @@ cpp_dec_float_50* Sk(float* Ejs , int q , std::complex<float> D, int kmax){
         p_coeffs[0] = 0.0;
         // Multiplication by the root E_i:
         for(int j = 0; j < i + 1; j++){
-        p_coeffs[j] += Ejs[i]*p_coeffs[j+1];
+            p_coeffs[j] += -1.0*Ejs[i]*p_coeffs[j+1];
         }
     }
     // Adding the (-1)^q D to the original polynomial:
@@ -50,7 +50,7 @@ cpp_dec_float_50* Sk(float* Ejs , int q , std::complex<float> D, int kmax){
         divided[i] = p_der_coeffs[i];
     }
     for(int i=0; i < kmax; i++){
-        a = divided[q-2]/p_coeffs[q-1];
+        a = divided[q-1]/p_coeffs[q];
         for(int j=0; j < q+1; j++){
             if(j == 0){
                 der_array[j] = -1.0*a*p_coeffs[j];
@@ -72,6 +72,7 @@ int main()
     float Energies[] = {-1.67, 2.5, 4.87, 7.8};
     int q = sizeof(Energies)/sizeof(float);
     int kmax = 6;
+    // ---------------------------------------------
     std::complex<float> D = {1.5, 0};
     cpp_dec_float_50* S_k_array = Sk(Energies , q , D , kmax);
     for(int i=0; i<kmax; i++){
